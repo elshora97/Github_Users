82a439d861a6ff4ae42b11bbda02b0d2dbf6a149
elshora97 <A.elshora@dataserve.com.sa>
2023-09-19 02:11:06 +0300

    Added PIE Chart Feature

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext, useGlobalContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useGlobalContext();
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  languages = Object.values(languages).sort((a, b) => {
+    return b.value - a.value;
+  });
+  console.log(languages);
+
+  const chartData = [
+    {
+      label: "javascript",
+      value: "20",
+    },
+    {
+      label: "css",
+      value: "26",
+    },
+    {
+      label: "html",
+      value: "18",
+    },
+  ];
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Pie3D data={languages} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
